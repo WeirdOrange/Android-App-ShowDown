e04c1dcf6bfbc0297226efcee223ef6a118afc7a
WeirdOrange <ivanlai2005@gmail.com>
2025-11-08 23:48:54 +0800

    Fixed issues in build.gradle and internal settings

--- showdown/app/build.gradle.kts
+++ showdown/app/build.gradle.kts
@@ -30,7 +30,17 @@ android {
         val mailPassword = localProperties.getProperty("MAIL_APP_PASSWORD") ?: ""
         buildConfigField("String", "MAIL_ADDRESS", "\"$mailAddress\"")
         buildConfigField("String", "MAIL_APP_PASSWORD", "\"$mailPassword\"")
+    }
 
+    packaging {
+        resources {
+            excludes += setOf(
+                "META-INF/NOTICE.md",
+                "META-INF/LICENSE.md",
+                "META-INF/NOTICE",
+                "META-INF/LICENSE"
+            )
+        }
     }
 
     buildTypes {
@@ -48,27 +58,30 @@ android {
     }
 }
 
-val roomVersion = "2.6.1"
-
 dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation("pl.droidsonroids.gif:android-gif-drawable:1.2.29")
-    implementation("org.jetbrains:annotations:23.0.0")
+    implementation(libs.gif.drawable)
+    implementation(libs.annotations)
+    implementation(libs.circle.imageview)
+    implementation(libs.recyclerview)
+    implementation(libs.coordinatorlayout)
 
-    implementation("de.hdodenhof:circleimageview:3.1.0")
-    implementation("androidx.recyclerview:recyclerview:1.3.1")
-    implementation("androidx.coordinatorlayout:coordinatorlayout:1.2.0")
+    // Room
+    implementation(libs.room.runtime)
+    annotationProcessor(libs.room.compiler)
 
-    implementation("androidx.room:room-runtime:${roomVersion}")
-    implementation ("com.sun.mail:android-mail:1.6.7")
-    implementation("com.sun.mail:android-activation:1.6.7")
+    // Mail
+    implementation(libs.android.mail)
+    implementation(libs.android.activation)
+
+    // Google Maps
     implementation(libs.play.services.maps)
-    testImplementation(libs.junit)
 
-    annotationProcessor("androidx.room:room-compiler:${roomVersion}")
+    // Testing
+    testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 }
--- showdown/settings.gradle.kts
+++ showdown/settings.gradle.kts
@@ -13,6 +13,10 @@ pluginManagement {
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
 
 rootProject.name = "showdown"
